aaa0a0619ced16be8b0b2b5571f900b3c3d35243
LeandroPrinos <32394153+LeandroPrinos@users.noreply.github.com>
2017-12-05 10:03:23 -0500

    blue

--- quick maffs/Form1.Designer.vb
+++ quick maffs/Form1.Designer.vb
@@ -89,6 +89,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
         Me.ClientSize = New System.Drawing.Size(503, 261)
         Me.Controls.Add(Me.btnSoumettre)
         Me.Controls.Add(Me.Label4)
