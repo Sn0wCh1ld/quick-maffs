602e170fc0e3cc90e03157f9baab8d370512fc3a
Justin Proulx (aka Sn0wCh1ld) <jawproulx2001@icloud.com>
2017-12-06 10:36:43 -0500

    game

--- quick maffs/Form1.vb
+++ quick maffs/Form1.vb
@@ -1,6 +1,55 @@
-﻿Public Class Form1
+﻿Public Class frmQM
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    Dim numQuestion As Integer
+    Dim réponse As Integer
 
+    Private Sub frmQM_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'commencer avec la première question
+        numQuestion = 1
+        Dim premièreRéponse As Integer = premièreQuestion()
+        réponse = premièreRéponse
     End Sub
+
+    Private Function premièreQuestion()
+        Dim numGauche As Integer = 1
+        Dim numDroit As Integer = 1
+
+        'calculer la réponse
+        Return calculerRéponse(numGauche, numDroit)
+    End Function
+
+    Private Function calculerRéponse(ByVal numGauche, ByVal numDroit)
+        Return (numGauche Mod numDroit)
+    End Function
+
+    Private Sub btnSoumettre_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSoumettre.Click
+        Dim réponseDonné As Integer = Val(txtReste.Text)
+
+        If réponseDonné = réponse Then
+            MsgBox("Bonne réponse")
+        Else
+            MsgBox("Mauvaise réponse")
+        End If
+
+        numQuestion += 1
+
+        nouvelleQuestion()
+    End Sub
+
+    Private Sub nouvelleQuestion()
+        Dim numDroit As Integer = auHazard()
+        Dim numGauche As Integer = auHazard() + numDroit
+
+        réponse = calculerRéponse(numGauche, numDroit)
+
+        lblNombreGauche.Text = numGauche
+        lblNombreDroit.Text = numDroit
+    End Sub
+
+    Private Function auHazard()
+        Randomize()
+
+        Dim nombre As Integer = CInt(Math.Floor((numQuestion * 2 - 1 + 1) * Rnd())) + 1
+        Return nombre
+    End Function
 End Class
