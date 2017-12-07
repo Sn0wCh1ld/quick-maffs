d6b3f50e734a2370e947bcefbb029551d74d1ee1
LeandroPrinos <32394153+LeandroPrinos@users.noreply.github.com>
2017-12-07 14:38:54 -0500

    int 2.0

--- quick maffs/Form1.vb
+++ quick maffs/Form1.vb
@@ -1,16 +1,16 @@
 ﻿Public Class frmQM
 
-    Dim numQuestion As Integer
-    Dim réponse As Integer
+    Dim intNumQuestion As Integer
+    Dim intRéponse As Integer
     Dim intDonnées(0, 0) As Integer
     Dim intBonneRéponse() As Integer
     Dim intRéponsesDonnées() As Integer
 
     Private Sub frmQM_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'commencer avec la première question
-        numQuestion = 1
+        intNumQuestion = 1
         Dim premièreRéponse As Integer = premièreQuestion()
-        réponse = premièreRéponse
+        intRéponse = premièreRéponse
     End Sub
 
     Private Function premièreQuestion()
@@ -28,25 +28,25 @@
     Private Sub btnSoumettre_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSoumettre.Click
         Dim réponseDonné As Integer = Val(txtReste.Text)
 
-        If réponseDonné = réponse Then
+        If réponseDonné = intRéponse Then
             MsgBox("Bonne réponse")
         Else
             MsgBox("Mauvaise réponse")
         End If
 
-        ReDim Preserve intBonneRéponse(numQuestion - 1)
-        ReDim Preserve intRéponsesDonnées(numQuestion - 1)
-        intBonneRéponse(numQuestion - 1) = réponse
-        intRéponsesDonnées(numQuestion - 1) = réponseDonné
+        ReDim Preserve intBonneRéponse(intNumQuestion - 1)
+        ReDim Preserve intRéponsesDonnées(intNumQuestion - 1)
+        intBonneRéponse(intNumQuestion - 1) = intRéponse
+        intRéponsesDonnées(intNumQuestion - 1) = réponseDonné
 
-        ReDim intDonnées(numQuestion - 1, 1)
+        ReDim intDonnées(intNumQuestion - 1, 1)
         For Each item As Integer In intBonneRéponse
             intDonnées(Array.IndexOf(intBonneRéponse, item), 0) = item
             intDonnées(Array.IndexOf(intBonneRéponse, item), 1) = intRéponsesDonnées(Array.IndexOf(intBonneRéponse, item))
         Next
 
         'incrémenter le nombre de la question
-        numQuestion = numQuestion + 1
+        intNumQuestion = intNumQuestion + 1
 
         'chercher une nouvelle question
         nouvelleQuestion()
@@ -56,7 +56,7 @@
         Dim numDroit As Integer = auHazard()
         Dim numGauche As Integer = auHazard() + numDroit
 
-        réponse = calculerRéponse(numGauche, numDroit)
+        intRéponse = calculerRéponse(numGauche, numDroit)
 
         lblNombreGauche.Text = numGauche
         lblNombreDroit.Text = numDroit
@@ -65,7 +65,7 @@
     Private Function auHazard()
         Randomize()
 
-        Dim nombre As Integer = CInt(Math.Floor((numQuestion * 2) * Rnd())) + 1
+        Dim nombre As Integer = CInt(Math.Floor((intNumQuestion * 2) * Rnd())) + 1
         Return nombre
     End Function
 End Class
